bbc1cc6b1385bbdafc0a561c009c19b9868ee646
PAWAN KUMAR TRIPATHI <78968923+Pawan-Kumar-Tripathi@users.noreply.github.com>
2022-06-27 12:19:59 +0530

    Add files via upload

---
+++ digitpattern.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+class digitpattern
+    {
+        public void main()
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Dear , user please enter the number ");
+            int n = sc.nextInt();
+            
+            for(;n!= 0;)
+            {
+                System.out.println(n);
+                
+                int rem = n%10;
+            }
+        }
+    }
+                
+                
+                
+                
+            
+            
